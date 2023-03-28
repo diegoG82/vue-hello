e26b8cec84e869df45c1dcd69e82323a816fcb78
diegoG82 <diegogiardina@hotmail.it>
2023-03-28 17:29:36 +0200

    Inserto imput e method

--- js/script.js
+++ js/script.js
@@ -9,10 +9,22 @@ createApp({
       //   INSERIMENTO IMMAGINE
       logovueimg:
         "https://css-tricks.com/wp-content/uploads/2020/06/vue-glowing.png",
+      
+        nameImput: "",
+      // INTERFACCIA CLASSE VUE CON CSS
+      imgclass: "imgwidth",
+      hiddenClass: "hidden",
     };
   },
-  //   METHODS AND EVENT HANDLING
-}).mount("#app");
 
-// Bonus:
-// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+  //   METHODS AND EVENT HANDLING
+  methods: {
+    hiddenText() {
+      if (this.hiddenClass === "") {
+        this.hiddenClass = "hidden";
+      } else {
+        this.hiddenClass = "";
+      }
+    },
+  },
+}).mount("#app");
