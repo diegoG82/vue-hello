0d9f8881cad23a610e2c0088597972209334f682
diegoG82 <diegogiardina@hotmail.it>
2023-03-28 16:13:23 +0200

    Inserita immagine

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 // INIZIALIZZO VUE
-
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      message: "Hello Vue!"
-    }
+      //   SEMPLICE MESSAGGIO
+      message: "Hello Vue!",
+      //   INSERIMENTO IMMAGINE
+      logovueimg:
+        "https://css-tricks.com/wp-content/uploads/2020/06/vue-glowing.png",
+    };
   },
+  //   METHODS AND EVENT HANDLING
 }).mount("#app");
 
-
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
